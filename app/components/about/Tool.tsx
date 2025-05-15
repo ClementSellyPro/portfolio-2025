7b70295ebf2c6804c2021637220e32f16522aac2
ClementSellyPro <clementselly.pro@gmail.com>
2025-05-15 08:35:57 +0400

    Reduce Tool size to improve Mobile screen version

--- app/components/about/Tool.tsx
+++ app/components/about/Tool.tsx
@@ -4,7 +4,7 @@ import { ToolType } from '@/app/type/ToolType';
 export default function Tool({name, color, logo}: ToolType) {
 
   return (
-    <div className="flex flex-col justify-center items-center gap-5 w-full h-[180px] relative hover:brightness-105 transition-all duration-200" style={{ backgroundColor: color}}>
+    <div className="flex flex-col justify-center items-center gap-5 w-full lg:h-[180px] h-[150px] relative hover:brightness-105 transition-all duration-200" style={{ backgroundColor: color}}>
       <Image src={logo} alt='Technologies' width={name === 'Figma' || name === 'TypeScript' ? 40 : 70} height={name === 'Figma' || name === 'TypeScript' ? 40 : 70} />
       <p className='tool-title absolute bottom-4'>{name}</p>
     </div>
